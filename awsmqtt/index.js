000208f4aed099493db1025c8102f8c54e4fe994
anojalax <anojalax@gmail.com>
2016-11-05 21:24:05 -0700

    Update index.js

--- awsmqtt/index.js
+++ awsmqtt/index.js
@@ -7,6 +7,7 @@ var client = mqtt.connect('mqtt://ec2-35-161-110-220.us-west-2.compute.amazonaws
 var board = new five.Board({
     io: new Edison()
 });
+var button;
 
 
 client.on('connect', function() {
@@ -18,21 +19,41 @@ client.on('connect', function() {
 
     board.on("ready", function() {
         var led = new five.Led(9);
-
-        client.on('message', function(topic, message) {
+        
+        board = new five.Board();
+     /*  client.on('message', function(topic, message) {
             // message is Buffer 
-            console.log(message.toString() );
-            if(message.toString() === "ledon"){
-            	led.on();
-            }else if(message.toString() === "ledoff"){
-            	led.off();
-            }else if(message.toString() === "ledfadein"){
-            	led.fadeIn(500);
-            }else if(message.toString() === "ledfadeout"){
-            	led.fadeOut(500);
-            }
+            console.log(message.toString() );        
+        });*/
+         
+          board.on("ready", function() {
 
-        });
+          // Create a new `button` hardware instance.
+          button = new five.Button(2);
+          board.repl.inject({
+            button: button
+          });
+
+          // Button Event API
+
+          // "down" the button is pressed
+          button.on("down", function() {
+            console.log("down");
+          });
+
+          // "hold" the button is pressed for specified time.
+          //        defaults to 500ms (1/2 second)
+          //        set
+          button.on("hold", function() {
+            console.log("hold");
+              led.on();
+          });
+
+          // "up" the button is released
+          button.on("up", function() {
+            console.log("up");
+          });
+        
 
     });
 
